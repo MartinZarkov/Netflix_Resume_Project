27a1caf6cb5178ae32b4de576a330f7e8999f5db
MartinZarkov <martinzarkov95@gmail.com>
2023-03-05 20:33:01 -0500

    header logout fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,13 @@ import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import useAuth from '../hooks/useAuth'
+import BasicMenu from './BasicMenu'
+import AccountImage from '../assets/account_img.png'
+import NetflixLogo from '../assets/Netflix_2015_logo.png'
 
 function Header() {
     const [isScrolled, setIsScrolled] = useState(false)
-    const { logout} = useAuth()
+    const {logout} = useAuth()
 
     useEffect(()=>{
         const handleScroll = () =>{
@@ -28,11 +31,13 @@ function Header() {
     <header className={`${isScrolled && 'bg-[#141414]'}`}>
         <div className="flex items-center space-x-2 md:space-x-10">
             <img
-            src="https://rb.gy/ulxxee"
+            src={NetflixLogo.src}
             width={100}
             height={100}
             className="cursor-pointer object-contain"
             />
+
+            <BasicMenu/>
 
             <ul className="hidden space-x-4 md:flex">
                 <li className="headerLink">Home</li>
@@ -50,12 +55,8 @@ function Header() {
 
             <BellIcon className='h-6 w-6'/>
 
-            <Link href="/account">
-                <img
-                    src="https://rb.gy/g1pwyx"
-                    alt=""
-                    className="cursor-pointer rounded"
-                />
+            <Link href="#">
+                <p className="cursor-pointer rounded hover:text-[#E50914]" onClick={logout}>Logout</p>
             </Link>
         </div>
     </header>
